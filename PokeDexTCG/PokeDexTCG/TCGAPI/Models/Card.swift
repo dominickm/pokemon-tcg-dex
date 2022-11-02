16b5a49a2200b1b7582d7583d1cc9769441bb7c5
Michael Dominick <michael@themadbotter.com>
2022-11-01 22:54:08 -0400

    changes to card model to comply with Decodable

--- PokeDexTCG/PokeDexTCG/TCGAPI/Models/Card.swift
+++ PokeDexTCG/PokeDexTCG/TCGAPI/Models/Card.swift
@@ -9,13 +9,13 @@ import Foundation
 
 // For now we assume all cards are Pokemon, because that's all the boy cares about :)
 
-struct Card {
+struct Card: Decodable {
     var id: String
     var name: String
     var hp: String
-    var types: any Collection<String>
+    var types: Array<String>
     var evolvesFrom: String?
-    var evolvesTo: any Collection<String>  // we assume a list of len 0 is a final evolve state
+    var evolvesTo: Array<String>  // we assume a list of len 0 is a final evolve state
     var images: Dictionary<String, URL>
     
     var lowResImageURL: URL? {
