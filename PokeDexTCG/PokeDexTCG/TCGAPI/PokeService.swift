9f76ed829edd27491c493d9f32a7a45cd138910b
Michael Dominick <michael@themadbotter.com>
2022-11-01 21:47:22 -0400

    a little naming hygeine

--- PokeDexTCG/PokeDexTCG/TCGAPI/PokeService.swift
+++ PokeDexTCG/PokeDexTCG/TCGAPI/PokeService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class PokeService {
     
-    let sharedPokeService = PokeService(apiKey: Bundle.main.infoDictionary?["POKE_KEY"] as? String)
+    let instance = PokeService(apiKey: Bundle.main.infoDictionary?["POKE_KEY"] as? String)
 
     let baseUrl = "https://api.pokemontcg.io/v2/cards/"
     let apiKey: String?
