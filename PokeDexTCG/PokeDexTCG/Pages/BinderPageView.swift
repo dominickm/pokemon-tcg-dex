03f858e59f8c5bbb5f0fb0ab354feb41583c5a82
Michael Dominick <michael@themadbotter.com>
2023-02-03 18:45:46 -0500

    incremental

---
+++ PokeDexTCG/PokeDexTCG/Pages/BinderPageView.swift
@@ -0,0 +1,27 @@
+//
+//  BinderPageView.swift
+//  PokeDexTCG
+//
+//  Created by mike on 2/3/23.
+//
+
+import SwiftUI
+
+struct BinderPageView: View {
+    var body: some View {
+        Grid() {
+            GridRow {
+                
+            }
+            GridRow {
+                
+            }
+        }
+    }
+}
+
+struct BinderPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BinderPageView()
+    }
+}
