2d97eb8a40b731e12207dd01db2417c13355036b
Michael Dominick <michael@themadbotter.com>
2023-02-03 20:53:57 -0500

    Added CardFrontView

---
+++ PokeDexTCG/PokeDexTCG/Views/CardFrontView.swift
@@ -0,0 +1,18 @@
+//
+//  CardFrontView.swift
+//  PokeDexTCG
+//
+//  Created by mike on 2/3/23.
+//
+
+import SwiftUI
+
+struct CardFrontView: View {
+    
+}
+
+struct CardFrontView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardFrontView()
+    }
+}
