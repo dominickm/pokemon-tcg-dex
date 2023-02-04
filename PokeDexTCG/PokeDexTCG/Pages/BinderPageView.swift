31f25a67b928d0174deb35026ebbbf1ed237e699
Michael Dominick <michael@themadbotter.com>
2023-02-03 22:54:08 -0500

    Added Poke-ball-esqcue gradient

--- PokeDexTCG/PokeDexTCG/Pages/BinderPageView.swift
+++ PokeDexTCG/PokeDexTCG/Pages/BinderPageView.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct BinderPageView: View {
+    @State private var query = ""
+    
     var body: some View {
-        Grid() {
-            GridRow {
-                CardView(cardId: "xy7-5")
-                CardView(cardId: "det1-4")
-                CardView(cardId: "xy2-1")
+        VStack {
+            Grid() {
+                GridRow {
+                    CardView(cardId: "xy7-5")
+                    CardView(cardId: "det1-4")
+                    CardView(cardId: "xy2-1")
+                }
+                GridRow {
+                    CardView(cardId: "pl1-1")
+                    CardView(cardId: "mcd19-1")
+                    CardView(cardId: "xy1-1")
+                }
             }
-            GridRow {
-                CardView(cardId: "pl1-1")
-                CardView(cardId: "mcd19-1")
-                CardView(cardId: "xy1-1")
-            }
-        }.background(Image("charizard-bg")
-            .resizable()
-            .scaledToFill())
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(LinearGradient(gradient: Gradient(colors: [.red, .gray, .white]), startPoint: .top, endPoint: .bottom))
     }
 }
+
 
 struct BinderPageView_Previews: PreviewProvider {
     static var previews: some View {
